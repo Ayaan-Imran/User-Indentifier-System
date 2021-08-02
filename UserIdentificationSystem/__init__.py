30a008e5e38ea4c0b0f29824f5355415b6ea5b72
Ayaan Imran <miskiacuberayaan2509@gmail.com>
2021-08-02 15:39:54 +0300

    Errors and bugs fixed and is finalized for publishing to pypi

--- UserIdentificationSystem/__init__.py
+++ UserIdentificationSystem/__init__.py
@@ -1,16 +1,14 @@
 import sqlite3
-import secrets
-import hashlib
-import getpass
+import passtools
 
-def encryt(prompt:str):
-    return hashlib.sha256(bytes(prompt, "utf-8")).hexdigest()
+__version__ = "0.9.0"
+__doc__ = "User Identification System is a package that will allow you to create complex login and registeration system in a simple way"
 
 class Basic():
     __connection = None
     __c = None
 
-    def __init__(self, filename:str):
+    def __init__(self, filename: str):
         self.username = None
 
         global __connection
@@ -19,9 +17,9 @@ class Basic():
         # Clean the filename
         if filename[-3:] != ".db":
             filename = filename + ".db"
-        self.filename = filename # Make the filename variable global within the instance
+        self.filename = filename  # Make the filename variable global within the instance
 
-        __connection = sqlite3.connect("{}.db".format(filename))
+        __connection = sqlite3.connect(filename)
         __c = __connection.cursor()
         __c.execute("""CREATE TABLE IF NOT EXISTS account (
             username text,
@@ -30,13 +28,13 @@ class Basic():
         """)
         __connection.commit()
 
-    def signup(self, username:str=None, password:str=None, autotask:bool=False):
+    def signup(self, username: str = None, password: str = None, autotask: bool = False):
         global __c
         global __connection
 
         if autotask == False:
-            if self.username_exists(username):
-                password = encryt(password)
+            if self.username_is_valid(username):
+                password = passtools.passhash(password)
                 __c.execute("INSERT INTO account VALUES(?,?)", (username, password))
                 __connection.commit()
                 return True
@@ -44,12 +42,12 @@ class Basic():
                 return False
         else:
             username1 = input("[+] * Please make a username: ")
-            password1 = encryt(getpass.getpass("[+] * Please make a password for security: "))
+            password1 = passtools.passask("[+] * Please make a password for security: ")
 
-            if self.username_is_valid(username1) == False: # If username is not valid, then ask again for another username
+            if self.username_is_valid(username1) == False:  # If username is not valid, then ask again for another username
                 while True:
                     username1 = input("[-] * The username you entered is already in use. Please make another one: ")
-                    if self.username_is_valid(username1): # If this function returns true, that means this username is valid
+                    if self.username_is_valid(username1):  # If this function returns true, that means this username is valid
                         break
 
             __c.execute("INSERT INTO account VALUES(?,?)", (username1, password1))
@@ -57,7 +55,7 @@ class Basic():
             self.username = username1
             return True
 
-    def login(self, username:str=None, password:str=None, autotask:bool=False):
+    def login(self, username: str = None, password: str = None, autotask: bool = False):
         global __c
         global __connection
 
@@ -66,7 +64,7 @@ class Basic():
             users = __c.fetchall()
             __connection.commit()
 
-            password = encryt(password)
+            password = passtools.passhash(password)
             permission = False
             for i in users:
                 if (i[0] == username) and (i[1] == password):
@@ -78,7 +76,7 @@ class Basic():
             username1 = input("[+] * Please enter your username: ")
             self.username = username1
 
-            password1 = encryt(input("[+] * Please enter your password: "))
+            password1 = passtools.passask("[+] * Please enter your password: ")
 
             __c.execute("SELECT * FROM account")
             users = __c.fetchall()
@@ -92,13 +90,13 @@ class Basic():
 
             return permission
 
-    def deluser(self, username:str=None, password:str=None, autotask:bool=False):
+    def deluser(self, username: str = None, password: str = None, autotask: bool = False):
         global __c
         global __connection
 
         test = Basic(self.filename)
         if autotask == False:
-            if test.login(username, password):
+            if test.login(username, password): # No need to has the password because login function already hashed the password
                 __c.execute("DELETE FROM account WHERE username = '{}'".format(username))
                 __connection.commit()
                 return True
@@ -108,10 +106,10 @@ class Basic():
             username = input("[+] * Please enter your username: ")
             self.username = username
 
-            password = input("[+] * Please enter your password for confirmation: ")
+            password = passtools.passask("[+] * Please enter your password for confirmation: ", do_hash=False)
 
             if test.login(username, password):
-                password = input("[+] * Please renter your password again for confirmation: ")
+                password = passtools.passask("[+] * Please renter your password again for confirmation: ", do_hash=False)
                 if test.login(username, password):
                     __c.execute("DELETE FROM account WHERE username = '{}'".format(username))
                     __connection.commit()
@@ -132,16 +130,16 @@ class Basic():
         lst = [i[0] for i in lst]
         return lst
 
-    def username_is_valid(self, username:str):
+    def username_is_valid(self, username: str):
         global __c
         global __connection
 
         lst = self.get_usernames()
 
-        if username in lst: # If username exists, then that means it is not valid
+        if username in lst:  # If username exists, then that means it is not valid
             return False
         else:
-            return True # If username does not exist, then that means username can be used and is valid
+            return True  # If username does not exist, then that means username can be used and is valid
 
     def secure(self):
         global __connection
@@ -152,7 +150,7 @@ class ExtraPass():
     __connection = None
     __c = None
 
-    def __init__(self, filename:str):
+    def __init__(self, filename: str):
         self.username = None
 
         global __connection
@@ -161,8 +159,9 @@ class ExtraPass():
         # Clean the filename
         if filename[-3:] != ".db":
             filename = filename + ".db"
-        self.filename = filename # Set the filename to be global within the instance
+        self.filename = filename  # Set the filename to be global within the instance
 
+        __connection = sqlite3.connect(self.filename)
         __c = __connection.cursor()
         __c.execute("""CREATE TABLE IF NOT EXISTS account (
             username text,
@@ -170,7 +169,7 @@ class ExtraPass():
             extra text
         )""")
 
-    def login(self, username:str=None, password:str=None, extra:str=None, autotask:bool=False):
+    def login(self, username: str = None, password: str = None, extra: str = None, autotask: bool = False):
         global __connection
         global __c
 
@@ -179,8 +178,8 @@ class ExtraPass():
             lst = __c.fetchall()
             __connection.commit()
 
-            password = encryt(password)
-            extra = encryt(extra)
+            password = passtools.passhash(password)
+            extra = passtools.passhash(extra)
 
             permission = False
             for i in lst:
@@ -191,8 +190,8 @@ class ExtraPass():
             return permission
         else:
             username = input("[+] * Please enter your username: ")
-            password = encryt(input("[+] * Please enter your password: "))
-            extra = encryt(input("[+] * Please enter the extra layer of password you added: "))
+            password = passtools.passask("[+] * Please enter your password: ")
+            extra = passtools.passask("[+] * Please enter the extra layer of password you added: ")
 
             __c.execute("SELECT * FROM account")
             lst = __c.fetchall()
@@ -208,29 +207,29 @@ class ExtraPass():
                 self.username = username
             return permission
 
-    def signup(self, username:str=None, password:str=None, extra:str=None, autotask:bool=False):
+    def signup(self, username: str = None, password: str = None, extra: str = None, autotask: bool = False):
         global __connection
         global __c
 
         if autotask == False:
-            if self.username_is_valid(username) == False: # If username is not valid, then it will return False
+            if self.username_is_valid(username) == False:  # If username is not valid, then it will return False
                 return False
             else:
-                password = encryt(password)
-                extra = encryt(extra)
+                password = passtools.passhash(password)
+                extra = passtools.passhash(extra)
 
                 __c.execute("INSERT INTO account VALUES (?,?,?)", (username, password, extra))
                 __connection.commit()
                 return True
         else:
             username = input("[+] * Please make a username: ")
-            password = encryt(input("[+] * Please make a password: "))
-            extra = encryt(input("[+] * Please enter another password that can be different for extra layer of security: "))
+            password = passtools.passask("[+] * Please make a password: ")
+            extra = passtools.passask("[+] * Please enter another password that can be different for extra layer of security: ")
 
-            if self.username_is_valid(username): # If username is not valid, then it will ask for a new one
+            if self.username_is_valid(username):  # If username is not valid, then it will ask for a new one
                 while True:
                     username = input("[-] * The username you entered is already in use. Please enter another one: ")
-                    if self.username_is_valid(username): # If username is valid then it will stop asking for a new one
+                    if self.username_is_valid(username):  # If username is valid then it will stop asking for a new one
                         break
                     else:
                         continue
@@ -240,13 +239,13 @@ class ExtraPass():
             self.username = username
             return True
 
-    def deluser(self, username:str=None, password:str=None, extra:str=None, autotask:bool=False):
+    def deluser(self, username: str = None, password: str = None, extra: str = None, autotask: bool = False):
         global __c
         global __connection
 
         test = ExtraPass(self.filename)
         if autotask == False:
-            if test.login(username, password, extra): # NOTE: No need for encryption before passing because login function already encypts the important variables.
+            if test.login(username, password, extra):  # NOTE: No need for encryption before passing because login function already encypts the important variables.
                 __c.execute("DELETE FROM account WHERE username = '{}'".format(username))
                 __connection.commit()
                 return True
@@ -255,8 +254,8 @@ class ExtraPass():
         else:
             # NOTE: No need for encryption before passing because login function already encypts the important variables.
             username = input("[+] * Please enter your username: ")
-            password = input("[+] * Please enter your password for confirmation: ")
-            extra = input("[+] * Please enter the password you gave for extra layer: ")
+            password = passtools.passask("[+] * Please enter your password for confirmation: ", do_hash=False)
+            extra = passtools.passask("[+] * Please enter the password you gave for extra layer: ", do_hash=False)
 
             if test.login(username, password, extra):
                 global username1
@@ -280,7 +279,7 @@ class ExtraPass():
         lst = [i[0] for i in lst]
         return lst
 
-    def username_is_valid(self, username:str):
+    def username_is_valid(self, username: str):
         global __connection
         global __c
 
@@ -294,37 +293,3 @@ class ExtraPass():
     def secure(self):
         global __connection
         __connection.close()
-
-
-def passgen(length:int=10, caplock:str="mix"):
-    # Define the letter, numbers and symbols
-    letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "h", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-               "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
-               "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-    symbols = ["@", "#", "%", "!", "*", ">", "<", "$"]
-
-    # Check if length parameter is valid
-    if length < 3:
-        raise ValueError("Length is too short. It has to be greater than 2")
-
-    # Create generator instance
-    generartor = secrets.SystemRandom()
-
-    # Create the random string
-    result = ""
-    turn = "letter"
-    for _ in length:
-        if turn == "letter":
-            result += generartor.choice(letters)
-        elif turn == "number":
-            result += generartor.choice(numbers)
-        else:
-            result += generartor.choice(symbols)
-
-    # Shuffle the randomized string
-    for _ in 10:
-        result = generartor.shuffle(result)
-
-    # Return the result
-    return result
